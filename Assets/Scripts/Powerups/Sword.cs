a6820fc29baf106b2a5a5fa46abd75972c143bdc
nikita <gudar23@gmail.com>
2021-05-08 18:13:11 +0300

    add sword

---
+++ Assets/Scripts/Powerups/Sword.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.Powerups
+{
+    public class Sword : Powerup
+    {
+        protected override void PickUp()
+        {
+            Debug.Log("Sword.PuckUp -> picked up a sword.");
+        }
+    }
+}
